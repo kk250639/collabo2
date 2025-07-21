ea03321928f3da9611ddd906a5ee35c834ae68df
kk250639 <chesyeo@gmail.com>
2025-07-21 10:38:31 +0900

    a

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("팀장이 작성한 코드");
-        System.out.println("기타 등등 필요한 파일 작성");
+
     }
 }
