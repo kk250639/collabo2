8edbf260355fa59370dee32057f6e7959d8ba43c
kk250639 <chesyeo@gmail.com>
2025-07-21 10:29:50 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("팀장이 작성한 코드");
+        System.out.println("기타 등등 필요한 파일 작성");
+    }
+}
